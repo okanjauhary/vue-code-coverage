7507d1cc7d9c0bd984646e61482fd506637ec2c7
Sulhan Jauhary <ms.jauhary@gmail.com>
2019-12-01 14:22:32 +0700

    first init

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{vue,jsx}',
+    '!**/node_modules/**',
+    '!<rootDir>/dist/**',
+    '!<rootDir>/src/plugins/**',
+    '!<rootDir>/tests/unit/**'
+  ],
+  coverageReporters: ['lcov', 'text-summary']
 }
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,11 +2,23 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+  const wrapper = shallowMount(HelloWorld)
+  it('Register is component', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('count is initially 1', () => {
+    expect(wrapper.vm.count).toBe(1)
+  })
+
+  it('Button is present', () => {
+    expect(wrapper.contains('button')).toBeTruthy()
+  })
+
+  it('count is increment 1 was button clicked', () => {
+    const expected = 1
+    const button = wrapper.find('button')
+    button.trigger('click')
+    expect(wrapper.vm.count).toBe(2)
   })
 })
